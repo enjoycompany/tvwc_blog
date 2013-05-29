2cf6e56901437b7e2b05058b48d55c373f04a813
enjoycompany <enjoycompany@gmail.com>
2013-05-29 15:22:30 +1000

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'jquery-rails'
+#gem 'devise'
+#gem 'simple_form'
+
+group :production do
+	gem 'pg'
+end
+group :development, :test do
+	gem 'sqlite3', '1.3.7'
+end
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  #gem 'bootstrap-sass', '~> 2.2.2.0'
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TvwcBlog::Application.config.secret_token = '83a3ab332a4d0a446c51a6ffd51cbaab69b5467e43a94894fa679312323aeb2d8e62e0fb9c4a5cdf62f8c7e49df25cc04b1f7ecc748e662e760b8d2751e9cd53'
